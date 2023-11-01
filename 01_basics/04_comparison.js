c4bc0484ba53d303386bbbd0530e7c81124c0c3d
Rohit Mhatre <101646263+rohit-mhatre@users.noreply.github.com>
2023-11-01 15:46:48 +0000

    updated 04_comparison.js

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,25 @@
+//traditional comparison:
+console.log(1 > 4);
+console.log(1 >= 4);
+console.log(1 < 4);
+console.log(1 == 4);
+console.log(1 != 4);
+
+//these will just give out the result in true or false format
+
+console.log("2" > 1)
+console.log(1 < "31")
+
+//the comparison with null and undefined are complex, just study about them once
+
+//strict compaison (===):
+console.log(3 === "3")
+console.log(3 === 3)
+
+/**Strict comparison in JavaScript, often denoted as ===, is a type of equality comparison that not only checks if the values are equal but also ensures that the data types are the same. 
+ * In a strict comparison:
+    Two values are considered equal only if they have the same value and the same data type.
+    If the data types are different, the values are considered not equal.
+    It does not perform type coercion, meaning it does not attempt to convert the values to the same data type for comparison.
+ */
+
