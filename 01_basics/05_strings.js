0b70f49dd34c9d4a5cdcf435cd143221f4437705
Rohit Mhatre <101646263+rohit-mhatre@users.noreply.github.com>
2023-11-01 16:32:28 +0000

    updated 05_strings.js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,10 @@
+//string interpolation:
+let pname = "Ousmane Dembele"
+let team = "barcelona"
+
+console.log(`I am ${pname} and i play for ${team}`);
+
+console.log("rohit" === "Rohit")            //case sensitive, so if you do have to compare just convert them into same case
+
+//read string methods from the reference.
+//reference : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
