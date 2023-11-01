87f20a5ca694f2db1e3f9d08138af1f0a8f842de
Rohit Mhatre <101646263+rohit-mhatre@users.noreply.github.com>
2023-11-01 15:41:26 +0000

    updated 03_conversionOperations.js

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,18 +1,18 @@
-// let goals = 3
-// let goals = "3"
-// let variable = "abc"
-// let goalsInNumber = Number(goals)
-// let variableInNumber = Number(variable)
-// console.log(typeof goals)
-// console.log(typeof goalsInNumber)
-// console.log(goalsInNumber)
-// console.log(typeof variableInNumber)
-// console.log(variableInNumber)       //will output NaN not a number
+let goals = 3
+let goals = "3"
+let variable = "abc"
+let goalsInNumber = Number(goals)
+let variableInNumber = Number(variable)
+console.log(typeof goals)
+console.log(typeof goalsInNumber)
+console.log(goalsInNumber)
+console.log(typeof variableInNumber)
+console.log(variableInNumber)       //will output NaN not a number
 
-// let isLoggedIn = 23
-// let BooleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(typeof BooleanIsLoggedIn)
-// console.log(BooleanIsLoggedIn)
+let isLoggedIn = 23
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof BooleanIsLoggedIn)
+console.log(BooleanIsLoggedIn)
 /*  See here are few cases:
     if there is string and its empty and you convert it to boolean it would be false
     every other case it would be true
